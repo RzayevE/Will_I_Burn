d5db296e7092523ffdf6f778d6b794ae4746accf
Elnur <eerzayev95@gmail.com>
2019-09-11 12:02:48 +0200

    Initial Commit

---
+++ Will I Burn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Will I Burn
+//
+//  Created by Elnur Rzayev on 11/09/2019.
+//  Copyright © 2019 Elnur Rzayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
